94df19e7515eacd58812ffb8d936bc4421a35cb7
bungaauulia <bungaaulia1215@gmail.com>
2025-02-20 15:11:30 +0700

    commit lat praktikum matkul

---
+++ MataKuliah04.java
@@ -0,0 +1,41 @@
+public class MataKuliah04 {
+
+    String kodeMK;
+    String nama;
+    int sks;
+    int jumlahJam;
+
+    void tampilkanInformasi(){
+        System.out.println("Kode MK:" + kodeMK);
+        System.out.println("Nama MK: " + nama);
+        System.out.println("Jumlah SKS: " + sks);
+        System.out.println("Jumlah Jam: " + jumlahJam);
+    }
+
+    void ubahSKS(int sksBaru) {
+        sks = sksBaru;
+    }
+
+    void tambahJam(int tbhJam) {
+        jumlahJam += tbhJam;
+        System.out.println("Jumlah jam setelah ditambah: " + jumlahJam + " jam");
+    }
+
+    void kurangiJam(int krgJam) {
+        if (jumlahJam >= krgJam) {
+            jumlahJam -= krgJam;
+            System.out.println("Jumlah jam setelah dikurangi: " + jumlahJam + " jam");
+        } else {
+            System.out.println("Pengurangan tidak dapat dilakukan. Jumlah jam tidak mencukupi");
+        }
+    }
+public MataKuliah04() {
+
+}
+public MataKuliah04(String kodeMK, String nama, int sks, int jmlJam) {
+    this.kodeMK = kodeMK;
+    this.nama = nama;
+    this.sks = sks;
+    jumlahJam = jmlJam;
+}
+}
---
+++ MataKuliahMain04.java
@@ -0,0 +1,33 @@
+public class MataKuliahMain04 {
+    
+    public static void main(String[] args) {
+        
+        MataKuliah04 mk1 = new MataKuliah04();
+        mk1.kodeMK = "SIB242004";
+        mk1.nama = "Algoritma Struktur Data";
+        mk1.sks = 2;
+        mk1.jumlahJam = 6;
+       
+        mk1.tampilkanInformasi();
+        mk1.ubahSKS(4);
+        mk1.kurangiJam(3);
+        mk1.tampilkanInformasi();
+        System.out.println();
+
+        MataKuliah04 mk2 = new MataKuliah04("SIB242006", "Basis Data", 
+        2, 3);
+        mk2.tampilkanInformasi();
+        mk2.tambahJam(1);
+        mk2.tampilkanInformasi();
+        System.out.println();
+
+        MataKuliah04 mk3 = new MataKuliah04("SIB242008", "Matematika Lanjut", 
+        2, 2);
+        mk3.tampilkanInformasi();
+        mk3.kurangiJam(3);
+        System.out.println();
+
+    }
+
+
+}
